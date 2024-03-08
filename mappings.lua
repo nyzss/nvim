076d510a2df23720aae045e74181cc443eb5b85c
nyzss <oknakoca@gmail.com>
2024-03-08 23:42:06 +0100

    updated themes

--- mappings.lua
+++ mappings.lua
@@ -27,11 +27,7 @@ return {
       end,
       desc = "Pick to close",
     },
-    ["<F2>"] = {
-      --function() vim.cmd [[:!c_formatter_42 %:p<CR>]] end,
-      ":silent !c_formatter_42 %:p<cr>",
-      desc = "Format with c_formatter_42",
-    },
+    ["<F2>"] = { ":silent !c_formatter_42 %:p<cr>", desc = "Format with c_formatter_42" },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
---
+++ plugins/colorschemes.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    "AlphaTechnolog/pywal.nvim",
+    lazy = false,
+  },
+  {
+    "mbrea-c/wal-colors.nvim",
+    lazy = false,
+  },
+  {
+    "aktersnurra/no-clown-fiesta.nvim",
+    lazy = false,
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+  },
+  {
+    "sainnhe/everforest",
+    lazy = false,
+  },
+  {
+
+    "AhmedAbdulrahman/vim-aylin",
+    lazy = false,
+  },
+  {
+
+    "pineapplegiant/spaceduck",
+    lazy = false,
+  },
+  {
+
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+  },
+
+  {
+    "nanotech/jellybeans.vim",
+    lazy = false,
+  },
+  {
+    "lighthaus-theme/vim-lighthaus",
+    lazy = false,
+  },
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    init = function() vim.g.sonokai_dim_inactive_windows = 1 end,
+  },
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    lazy = false,
+  },
+  {
+    "jacoborus/tender.vim",
+    lazy = false,
+  },
+  {
+    "zootedb0t/citruszest.nvim",
+    lazy = false,
+  },
+  {
+    "cocopon/iceberg.vim",
+    lazy = false,
+  },
+  {
+    "luisiacc/gruvbox-baby",
+    lazy = false,
+  },
+  {
+    "rainglow/vim",
+    lazy = false,
+  },
+}
